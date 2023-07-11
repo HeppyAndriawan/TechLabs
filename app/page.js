edd47ca4ee892422bd297483cc0917c379fd32b6
OlyaLopatina <134008232+OlyaLopatina@users.noreply.github.com>
2023-07-11 22:51:29 +0200

    11.07.23
    I've created NavBar folder

--- app/page.js
+++ app/page.js
@@ -1,9 +1,10 @@
 "use client";
 import { useSession } from "next-auth/react";
+import NavBar from "@/components/NavBar/NavBar";
 
 export default function Home() {
   const { data: session, status } = useSession();
   // console.log(session)
 
-  return <>{/*  Please place your component after this lines below */}</>;
+  return <><NavBar/></>;
 }
