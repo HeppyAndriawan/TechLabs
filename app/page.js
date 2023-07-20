c95ae829e6d9bdaddbb47ff46f62d7ed24cbed60
OlyaLopatina <134008232+OlyaLopatina@users.noreply.github.com>
2023-07-20 21:13:04 +0200

    20.07.23 Olya -Routing task
    Working routing_Next.js

--- app/page.js
+++ app/page.js
@@ -2,6 +2,25 @@
 import { useSession } from "next-auth/react";
 import NavBar from "@/components/NavBar/NavBar";
 
+import Link from 'next/link'
+
+
+function login () {
+  return (
+    <ul>
+      <li>
+        <Link href="/my_account">My account</Link>
+      </li>
+      <li>
+        <Link href="/login">Log in</Link>
+      </li>
+      <li>
+        <Link href="/signup">Sign up</Link>
+      </li>
+    </ul>
+  )
+}
+
 export default function Home() {
   const { data: session, status } = useSession();
   // console.log(session)
--- components/NavBar/NavBar.jsx
+++ components/NavBar/NavBar.jsx
@@ -2,19 +2,26 @@ import React from 'react'
 import Link from 'next/link'
 
 
-function Home() {
+function Header () {
   return (
-    <ul>
-      <li>
-        <Link href="/my_account">My account</Link>
-      </li>
-      <li>
-        <Link href="/login">Log in</Link>
-      </li>
-      <li>
-        <Link href="/signup">Sign up</Link>
-      </li>
-    </ul>
+
+    <Link href="/login">Log in</Link>
+        
+  )
+}
+
+function Nav () {
+  return (
+    
+    <Link href="/my_account">My Account</Link>
+        
+  )
+}
+function Navi () {
+  return (
+    
+    <Link href="/signup">Sign up</Link>
+        
   )
 }
  
@@ -37,9 +44,9 @@ export default function NavBar() {
         </div>
         <div class="absolute right-0 hidden sm:ml-6 sm:block self-center">
           <div class="space-x-2.5 hover:space-x-8">
-            <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">My account</a>
-            <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Log in</a>
-            <a href="#" class="bg-white text-black rounded-md px-3 py-2 text-sm font-medium" aria-current="page">Sign up</a>
+            <a href="my_account" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">My account</a>
+            <a href="login" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Log in</a>
+            <a href="signup" class="bg-white text-black rounded-md px-3 py-2 text-sm font-medium" aria-current="page">Sign up</a>
           </div>
         </div>
       </div>
@@ -52,12 +59,12 @@ export default function NavBar() {
   <div class="sm:hidden" id="mobile-menu">
     <div class="space-y-1 px-2 pb-3 pt-2">
   
-      <a href="#" class="bg-gray-900 text-white block rounded-md px-3 py-2 text-base font-medium" aria-current="page">My account</a>
-      <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Log in</a>
-      <a href="#" class="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Sign up</a>
+      <a href="my_account" class="bg-gray-900 text-white block rounded-md px-3 py-2 text-base font-medium" aria-current="page">My account</a>
+      <a href="login" class="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Log in</a>
+      <a href="signup" class="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium">Sign up</a>
     </div>
   </div>
-</nav>
-    </div>
+  </nav>
+  </div>
   )
 }
