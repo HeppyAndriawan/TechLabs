807c17b79143db0278bf31dac55e034ad035e2c6
Heppy Andriawan <heppyandriawan@gmail.com>
2023-07-22 15:29:12 +0200

    last page.js 22 July 2023 15:29

--- app/page.js
+++ app/page.js
@@ -1,12 +1,20 @@
 "use client";
 import { useSession } from "next-auth/react";
 import Footer from "@/components/Footer/Footer";
+import Hero from "@/components/Hero/Hero";
+import { SwitchTo } from "@/tool/Switch/Switch";
 
 export default function Home() {
   const { data: session, status } = useSession();
-  // console.log(session)
+  console.log(status);
 
-  return <>{/*  Please place your component after this lines below */}
-   
-  </>;
+  return (
+    <>
+      {/*  Please place your component after this lines below */}
+      <SwitchTo condition={status === "authenticated"}>
+        <a></a>
+      </SwitchTo>
+      <Hero />
+    </>
+  );
 }
